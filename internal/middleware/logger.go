2687369f10373ff490b86ded4ff315b6ecc6774e
Manni Moghimi <mannimoghimi@gmail.com>
2023-11-22 23:02:33 +0330

    feat: added more info to logs

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,13 +1,17 @@
 package middleware
 
 import (
+    "os"
     "log"
+    "time"
     "net/http"
 )
 
 func NewLogger(l *log.Logger) middleware {
     return func(next http.Handler) http.Handler {
         return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+            start := time.Now()
+
             defer func() {
                 if r := recover(); r != nil {
                     l.Println(r)
@@ -15,6 +19,17 @@ func NewLogger(l *log.Logger) middleware {
             }()
 
             next.ServeHTTP(w, req)
+
+            hostname := os.Getenv("HOSTNAME")
+
+            log.Printf(
+                "%s %s %s %s %v",
+                hostname,
+                req.Method,
+                req.RequestURI,
+                req.Proto,
+                time.Since(start),
+            )
         })
     }
 }
